e620d79854cffabfcc352439b47e6b94e58c840d
Nik Bauer <njb26@case.edu>
2013-05-23 18:08:33 -0400

    added in empty activity for camera
    empty "hello world" activity added in. named CameraActivity

--- workspace/MemeApp/gen/com/uc/memeapp/R.java
+++ workspace/MemeApp/gen/com/uc/memeapp/R.java
@@ -32,21 +32,26 @@ public final class R {
         public static final int tv=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_selection=0x7f030001;
+        public static final int activity_camera=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_selection=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int selection=0x7f070001;
+        public static final int camera=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int selection=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int gallery=0x7f050003;
         public static final int grab=0x7f050007;
+        public static final int hello_world=0x7f050009;
+        public static final int photo=0x7f05000a;
         public static final int stock=0x7f050004;
         public static final int take=0x7f050006;
         public static final int title=0x7f050005;
+        public static final int title_activity_camera=0x7f050008;
         public static final int title_activity_selection=0x7f050002;
     }
     public static final class style {
