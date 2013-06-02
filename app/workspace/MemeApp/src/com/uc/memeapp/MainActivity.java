999f9cb88ee7f6221818f72b6b31b258ebf3acf3
Nik Bauer <njb26@case.edu>
2013-06-02 14:48:19 -0400

    updating activity main files- wonky confusion
    swtiched into curtis' branch and back into master, now it won't let me
    merge unless i commit these. merging to see if this solves the problem

--- app/workspace/MemeApp/src/com/uc/memeapp/MainActivity.java
+++ app/workspace/MemeApp/src/com/uc/memeapp/MainActivity.java
@@ -2,14 +2,25 @@ package com.uc.memeapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	private ImageButton takeButton;
+	private ImageButton selectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        takeButton = (ImageButton) findViewById(R.id.imgButton_take);
+        selectButton = (ImageButton) findViewById(R.id.imgButton_grab);
+        
+        takeButton.setOnClickListener(this);
+        selectButton.setOnClickListener(this);
     }
 
 
@@ -19,5 +30,24 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case (R.id.imgButton_grab):
+			Intent intent = new Intent(this, SelectionActivity.class);
+		startActivity(intent);
+		break;
+		
+		/*case R.id.imgButton_take:
+			Intent intent = new Intent(this, cameraActivity.class);
+			startActivity(intent);
+			break;*/
+		
+		}
+		
+	}
     
 }
